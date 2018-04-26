ce28aa9f9ff4f0c057b249f44338b08c7f9b3000
anoxil <bres.raphael@gmail.com>
2018-04-26 16:18:23 +0200

    auth biatch

--- MMM-Bordeaux-Transports.js
+++ MMM-Bordeaux-Transports.js
@@ -9,9 +9,10 @@ Module.register("MMM-Bordeaux-Transports", {
   // Definition des paramètres de configuration par défaut, accessibles via this.config.nomVariable
   defaults: {
     updateInterval: 1 * 60 * 1000, //temps de rafraichissement en ms
-    homeLatitude: 44.810787, //https://www.coordonnees-gps.fr
-    homeLongitude: -0.591106,
-    navitiaKey: "3cb5aaa3-2743-42f8-929c-b5f36ff64cb9"
+    homeLatitude: 44.815051, //https://www.coordonnees-gps.fr
+    homeLongitude: -0.588111,
+    navitiaKey: "3cb5aaa3-2743-42f8-929c-b5f36ff64cb9",
+    mdp: "fabien47"
   },
 
   // Récupère les feuilles de style (fontawesome pour les icones, à virer si inutile)
@@ -25,10 +26,17 @@ Module.register("MMM-Bordeaux-Transports", {
     console.log("AAAAAAAAAAAAAAAAAAA");
     var self = this;
 
+    this.sendConfig();
+
     //Rafraichissement de l'affichage et des informations
     setInterval(function() {
       self.updateDom();
     }, self.config.updateInterval);
+  },
+
+    //Envoie la configuration au node helper
+    sendConfig: function() {
+      this.sendSocketNotification("GET_CONFIG", this.config);
   },
 
   //Permet de convertir un horaire format timestamp donné par le calendrier Google en un format hh:mm:ss
@@ -36,6 +44,8 @@ Module.register("MMM-Bordeaux-Transports", {
     // Create a new JavaScript Date object based on the timestamp
     // multiplied by 1000 so that the argument is in milliseconds, not seconds.
     let date = new Date(timestamp * 1000);
+    // Days part from the timstamp
+    let day = date.getDate();
     // Hours part from the timestamp
     let hours = date.getHours();
     // Minutes part from the timestamp
@@ -46,6 +56,7 @@ Module.register("MMM-Bordeaux-Transports", {
     //(Possibilité de virer les secondes si inutile, ou bien de renvoyer un autre format)
     // Will display time in 10:30:23 format
     let formattedTime = hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
+    //let formattedTime = yearmonthday + "T" + hours + minutes + seconds;
 
     return formattedTime;
   },
@@ -54,7 +65,7 @@ Module.register("MMM-Bordeaux-Transports", {
   getDom: function() {
     const wrapper = document.createElement("div");
 
-    // ...
+
 
     return wrapper;
   },
@@ -63,7 +74,14 @@ Module.register("MMM-Bordeaux-Transports", {
   //Rappel : une notification est composée d'un nom (NOM_NOTIFICATION) et d'un payload (contenuNotification)
 
   // Gère les notifications INTERNES au module (interactions avec le node_helper)
-  socketNotificationReceived: function(notification, payload) {},
+  socketNotificationReceived: function(notification, payload) {
+    switch (notification) {
+      case "NAVITIA_RESULT" :
+        const navitia = payload;
+
+        console.log(navitia);
+    }
+  },
 
   //Gère les notifications inter-modules
   notificationReceived: function(notification, payload) {
@@ -76,8 +94,10 @@ Module.register("MMM-Bordeaux-Transports", {
         const event1Startdate = event1.startDate;
         const formattedStartDate = this.convertTimestamp(event1Startdate);
 
+        this.sendSocketNotification("UPDATE_EVENT_INFO", event1);
+
         //plus d'infos via :
-        //console.log(payload);
+        console.log(payload);
         break;
     }
   }
--- node_helper.js
+++ node_helper.js
@@ -8,26 +8,59 @@ module.exports = NodeHelper.create({
 
   // À exécuter lors du démarrage du miroir
   start: function() {
-    console.log("MMM-Bordeaux-Traansports helper started ...");
+    console.log("MMM-Bordeaux-Transports helper started ...");
   },
 
 
   //Exemple de requête via url
-  exempleRequest: function() {
-    let url ="myApiUrl";
-    var self = this;
+  navitiaRequest: function() {
 
-    request({ url: url }, function(error, response, body) {
+    let from = this.config.homeLatitude + "%3B" + this.config.homeLongitude;
+    let to = this.config.goTo_lat + "%3B" + this.config.goTo_lon;
+
+    // let url = "https://api.navitia.io/v1/coverage/fr-sw/journeys?from=" + from + "&to=" + to + "&datetime=" + this.config.beginTime + "&";
+    let url = "https://api.navitia.io/v1/coverage/fr-sw/journeys?from=-0.5880840000000001%3B44.815068&to=-0.5976359999999999%3B44.806078&";
+
+
+    var self = this;
+    /*
+    request() ({ url: url }, function(error, response, body) {
       if (error || response.statusCode !== 200) {
         return console.log(error || { statusCode: response.statusCode });
       }
       else {
         //Traiter notre réponse "response"
+        let payload = response;
 
+        console.log(payload);
         //On peut ensuite envoyer une notification au module (payload = contenu de la notification)
-        self.sendSocketNotification("EXAMPLE_NAME", examplePayload);
+        //self.sendSocketNotification("NAVITIA_RESULT", payload);
+      }
+    });*/
+
+
+    let options = {
+      url: url,
+      auth: {
+        username: this.config.navitiaKey,
+        password: this.config.mdp
+      }
+    };
+
+    request.get(options, function(error, response, body) {
+      if (error || response.statusCode !== 200) {
+        return console.log(error || { statusCode: response.statusCode });
+      }
+      else {
+        //Traiter notre réponse "response"
+        let payload = response;
+
+        console.log(payload);
+        //On peut ensuite envoyer une notification au module (payload = contenu de la notification)
+        //self.sendSocketNotification("NAVITIA_RESULT", payload);
       }
     });
+
   },
 
 
@@ -35,5 +68,20 @@ module.exports = NodeHelper.create({
   //Rappel : une notification est composée d'un nom (NOM_NOTIFICATION) et d'un payload (contenuNotification)
   //le mot clé "socket" indique que la notification est INTERNE au module 
   socketNotificationReceived: function(notification, payload) {
+    switch (notification) {
+      case "GET_CONFIG":
+        // On charge la config (coordonnées miroir, key) on start of module
+        this.config = payload;
+        break;
+
+      case "UPDATE_EVENT_INFO":        
+        // On charge la config
+        this.config.goTo_lat = 44.806287; //payload.goTo_lat;
+        this.config.goTo_lon = -0.596923; //payload.goTo_lon;
+        this.config.beginTime = "20180427T151700"; //payload.time;
+        //Et on exécute l'url de requete de navitia
+        this.navitiaRequest();
+        break;
   }
+}
 });
