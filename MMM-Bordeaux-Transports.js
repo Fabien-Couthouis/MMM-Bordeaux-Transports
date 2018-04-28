117dcb16bbdd24fb8f93dbb26a37d876711eab26
FABIEN-COUTHOUIS <couthouis.fabien@gmail.com>
2018-04-28 15:41:44 +0200

    Formattage des horaires de l'événement + récupération du lieu sous forme de données gps

--- MMM-Bordeaux-Transports.js
+++ MMM-Bordeaux-Transports.js
@@ -12,7 +12,8 @@ Module.register("MMM-Bordeaux-Transports", {
     homeLatitude: 44.815051, //https://www.coordonnees-gps.fr
     homeLongitude: -0.588111,
     navitiaKey: "3cb5aaa3-2743-42f8-929c-b5f36ff64cb9",
-    mdp: "fabien47"
+    navitiaPswd: "fabien47",
+    googleMapKey: "AIzaSyC1y4lpgvpsbjTxBczKKMjUamquldQR8AY",
   },
 
   // Récupère les feuilles de style (fontawesome pour les icones, à virer si inutile)
@@ -23,8 +24,7 @@ Module.register("MMM-Bordeaux-Transports", {
   // Comportement au démarrage
   start: function() {
     Log.info("Starting module: " + this.name);
-    console.log("AAAAAAAAAAAAAAAAAAA");
-    var self = this;
+    const self = this;
 
     this.sendConfig();
 
@@ -35,31 +35,10 @@ Module.register("MMM-Bordeaux-Transports", {
   },
 
     //Envoie la configuration au node helper
-    sendConfig: function() {
-      this.sendSocketNotification("GET_CONFIG", this.config);
+  sendConfig: function() {
+    this.sendSocketNotification("GET_CONFIG", this.config);
   },
 
-  //Permet de convertir un horaire format timestamp donné par le calendrier Google en un format hh:mm:ss
-  convertTimestamp: function(timestamp) {
-    // Create a new JavaScript Date object based on the timestamp
-    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-    let date = new Date(timestamp * 1000);
-    // Days part from the timstamp
-    let day = date.getDate();
-    // Hours part from the timestamp
-    let hours = date.getHours();
-    // Minutes part from the timestamp
-    let minutes = "0" + date.getMinutes();
-    // Seconds part from the timestamp
-    let seconds = "0" + date.getSeconds();
-
-    //(Possibilité de virer les secondes si inutile, ou bien de renvoyer un autre format)
-    // Will display time in 10:30:23 format
-    let formattedTime = hours + ":" + minutes.substr(-2) + ":" + seconds.substr(-2);
-    //let formattedTime = yearmonthday + "T" + hours + minutes + seconds;
-
-    return formattedTime;
-  },
 
   // Override le générateur du Dom, à changer pour l'affichage
   getDom: function() {
@@ -76,28 +55,26 @@ Module.register("MMM-Bordeaux-Transports", {
   // Gère les notifications INTERNES au module (interactions avec le node_helper)
   socketNotificationReceived: function(notification, payload) {
     switch (notification) {
+      case "EVENT_INFO_FORMATTED" :
+        const coordinates = payload;
+        this.sendSocketNotification("FETCH_NAVITIA", "");
+        break;
+
       case "NAVITIA_RESULT" :
         const navitia = payload;
-
-        console.log(navitia);
+        console.log(payload)
+        break;
     }
   },
+
+  
 
   //Gère les notifications inter-modules
   notificationReceived: function(notification, payload) {
     switch (notification) {
       case "CALENDAR_EVENTS":
-      //Exemple de traitement de la donnée events
-        const events = payload;
-        const event1 = events[0];
-        const event1Location = event1.location;
-        const event1Startdate = event1.startDate;
-        const formattedStartDate = this.convertTimestamp(event1Startdate);
-
-        this.sendSocketNotification("UPDATE_EVENT_INFO", event1);
-
-        //plus d'infos via :
-        console.log(payload);
+        const event1 = payload[0];
+        this.sendSocketNotification("FORMAT_EVENT_INFO", event1);
         break;
     }
   }
--- node_helper.js
+++ node_helper.js
@@ -9,58 +9,79 @@ module.exports = NodeHelper.create({
   // À exécuter lors du démarrage du miroir
   start: function() {
     console.log("MMM-Bordeaux-Transports helper started ...");
+    
+    // Info relatives au prochain événement)
+    this.event = {
+      beginTime: "",
+      arrival: ""
+    };    
+  },
+
+  //Met à jour this.event.arrival (coordonnées du lieu du prochain événement) en fonction de l'adresse donnée
+  updateGpsCoordinates(adress){
+    const self = this;
+    let url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + adress + "&key=" + this.config.googleMapKey;
+    url = encodeURI(url);
+
+    request.get(url, function(error, response, body) {
+      if (error || response.statusCode !== 200) {
+        return console.log("Error with Google Maps API request : " + error || { statusCode: response.statusCode });
+      }
+      else {
+        const doc = JSON.parse(body);
+        const location = doc.results[0].geometry.location;
+
+        self.event.arrival = {
+          lng: location.lng,
+          lat: location.lat
+        };
+
+        self.sendSocketNotification("EVENT_INFO_FORMATTED", this.arrival);
+      }
+    });
   },
 
 
-  //Exemple de requête via url
+  //Requête vers Navitia
   navitiaRequest: function() {
+    const self = this;
 
-    let from = this.config.homeLatitude + "%3B" + this.config.homeLongitude;
-    let to = this.config.goTo_lat + "%3B" + this.config.goTo_lon;
+    if (typeof this.event.arrival === "undefined"){
+      return console.log("Error : Unknown variable : this.event.lat. Update gps coordinate before using this function.");
+    }
 
-    // let url = "https://api.navitia.io/v1/coverage/fr-sw/journeys?from=" + from + "&to=" + to + "&datetime=" + this.config.beginTime + "&";
-    let url = "https://api.navitia.io/v1/coverage/fr-sw/journeys?from=-0.5880840000000001%3B44.815068&to=-0.5976359999999999%3B44.806078&";
-
-
-    var self = this;
-    /*
-    request() ({ url: url }, function(error, response, body) {
-      if (error || response.statusCode !== 200) {
-        return console.log(error || { statusCode: response.statusCode });
-      }
-      else {
-        //Traiter notre réponse "response"
-        let payload = response;
-
-        console.log(payload);
-        //On peut ensuite envoyer une notification au module (payload = contenu de la notification)
-        //self.sendSocketNotification("NAVITIA_RESULT", payload);
-      }
-    });*/
-
+    let from = this.config.homeLongitude + "%3B" + this.config.homeLatitude;
+    let to = this.event.arrival.lng + "%3B" + this.event.arrival.lat;
+    let url = "https://api.navitia.io/v1/coverage/fr-sw/journeys?from=" + from + "&to=" + to + "&datetime=" + this.event.beginTime + "&";
 
     let options = {
       url: url,
       auth: {
         username: this.config.navitiaKey,
-        password: this.config.mdp
+        password: this.config.navitiaPswd
       }
     };
 
     request.get(options, function(error, response, body) {
       if (error || response.statusCode !== 200) {
-        return console.log(error || { statusCode: response.statusCode });
+        return console.log("Error with Navitia request : " + error || { statusCode: response.statusCode });
       }
       else {
-        //Traiter notre réponse "response"
-        let payload = response;
-
-        console.log(payload);
+        //Traiter notre réponse 
+        //...
         //On peut ensuite envoyer une notification au module (payload = contenu de la notification)
-        //self.sendSocketNotification("NAVITIA_RESULT", payload);
+        self.sendSocketNotification("NAVITIA_RESULT", body);
       }
     });
 
+  },
+
+  //Permet de convertir un horaire format timestamp donné par le calendrier Google en un format hh:mm:ss
+  convertTimestamp: function(timestamp) {
+    const tzOffset = new Date().getTimezoneOffset() * 60000; 
+    const localISOTime = (new Date(timestamp - tzOffset)).toISOString().slice(0, -5);
+    const formattedForNavitia = localISOTime.replace(/:/gi,"").replace(/-/gi,"")
+    return formattedForNavitia;
   },
 
 
@@ -74,14 +95,18 @@ module.exports = NodeHelper.create({
         this.config = payload;
         break;
 
-      case "UPDATE_EVENT_INFO":        
-        // On charge la config
-        this.config.goTo_lat = 44.806287; //payload.goTo_lat;
-        this.config.goTo_lon = -0.596923; //payload.goTo_lon;
-        this.config.beginTime = "20180427T151700"; //payload.time;
-        //Et on exécute l'url de requete de navitia
+        //Met les informations de l'événement au bon format et les stock dans la variable this.event
+      case "FORMAT_EVENT_INFO":     
+        const event1 = payload;
+        this.event.beginTime = this.convertTimestamp(event1.startDate);
+        this.updateGpsCoordinates(event1.location);
+        break;
+
+      case "FETCH_NAVITIA":
+        //On exécute la requete vers navitia
         this.navitiaRequest();
         break;
+
   }
 }
 });
